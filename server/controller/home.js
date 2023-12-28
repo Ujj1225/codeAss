a246457b6614a41a28b156621e94b10a3e83c3a5
Ujjwal Jha <ujjwaljha1225@gmail.com>
2023-12-28 06:25:37 +0545

    Used gemini api to get simple setup ready

--- server/controller/home.js
+++ server/controller/home.js
@@ -1,6 +1,23 @@
-const home = (req, res) => {
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+require("dotenv").config();
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function api_call() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+  const prompt = "Write a story about magic stick";
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
+
+const home = async (req, res) => {
+  const display = await api_call();
   res.json({
     message: "Welcome first response of this API",
+    story: `${display}`,
   });
 };
 
