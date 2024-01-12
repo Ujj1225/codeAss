80a877470075f2233c2faddb8cb315c957d47048
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-01-12 19:43:57 +0545

    Created separate routes for optimize, review, translate

--- server/routes/query.js
+++ server/routes/query.js
@@ -1,10 +1,7 @@
 const express = require("express");
-const { home, optimize, review, translate } = require("../controller/home");
+const { optimize, translate, review } = require("../controller/query");
 const router = express.Router();
-
-router.route("/").get(home);
 router.route("/optimize").post(optimize);
-router.route("/review").post(review)
 router.route("/translate").post(translate);
-
+router.route("/review").post(review);
 module.exports = router;
